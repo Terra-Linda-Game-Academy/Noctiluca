0de340c2fd6632cdf62c3c21abd6255450eaeb3e
cooldeveloperperson <78451197+cooldeveloperperson@users.noreply.github.com>
2022-10-24 15:37:37 -0700

    fix 2

--- setup.bat
+++ setup.bat
@@ -3,7 +3,8 @@ rem get the location of the git executable from
 rem %localappdata%\GithubDesktop\app*\resources\app\git\cmd\git.exe
 
 set gitpath=%localappdata%\GithubDesktop\
-for /f "delims=" %%a in ('dir /b /ad %gitpath%\app*') do set gitpath=%gitpath%%a\resources\app\git\cmd\git.exe
+for /f "delims=" %%a in ('dir /b /ad %gitpath%\app*') do set app=%%a
+set gitpath=%gitpath%\%app%\resources\app\git\cmd\git.exe
 echo "git should be at %gitpath%"
 
 %gitpath% config core.hooksPath .githooks
