adf7331e081d8834bd25f9404673953d4e7f1cf8
Emerson Coskey <56370779+EmersonCoskey@users.noreply.github.com>
2022-10-28 15:57:40 -0700

    start working on level system

---
+++ Assets/Scripts/Levels/RoomData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace Levels {
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class RoomData : ScriptableObject {
+        
+    }
+}
---
+++ Assets/Scripts/Levels/TileEntity.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace Levels {
+    public abstract class TileEntity<T> : ScriptableObject {
+        protected Vector3Int pos;
+        protected TileDirection rot;
+        protected Vector3Int size; // not dimension, for example the length of the x side is (size.x + 1) and something with size 0 is 1x1x1
+
+        public Vector3Int Pos => pos;
+        public TileDirection Rot => rot;
+        public Vector3Int Size => size;
+        
+        public 
+    }
+}
