21b1e00c5180bdbd841af2a4be11602c48d32749
Evan Egan <89489191+EvanIsEpicAndCool@users.noreply.github.com>
2022-11-27 21:57:07 -0800

    finished
    fin/.

--- Assets/Scripts/Enemy/Burrower/playerDestroy.cs
+++ Assets/Scripts/Enemy/Burrower/playerDestroy.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class playerDestroy : MonoBehaviour
 {
+
+    public float aliveTime;
+    private void FixedUpdate()
+    {
+        aliveTime += Time.deltaTime;
+
+        if(aliveTime >= 5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag != "Enemy")
