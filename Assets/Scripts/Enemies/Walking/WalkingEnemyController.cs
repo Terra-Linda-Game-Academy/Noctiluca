cac2df3a25424caba4195f70051634fcb5cee861
Jackson Roberts <jacksongroberts@protonmail.com>
2022-12-09 12:46:42 -0800

    Finished walking enemy controller

--- Assets/Scripts/Enemies/Walking/WalkingEnemyController.cs
+++ Assets/Scripts/Enemies/Walking/WalkingEnemyController.cs
@@ -2,33 +2,67 @@ using UnityEngine;
 
 namespace Enemies.Walking {
 	public class WalkingEnemyController : MonoBehaviour {
-		public Vector3 playerPos;
+		public  Transform player;
+		private Vector3   _playerPos;
 
 		public bool  isWalking;
 		public float speed;
 		public float minDistance;
 
-		private float _distToPlayer;
+		public float maxViewRange     = 20.0f;
+		public float maxViewAngle     = 60.0f;
 
-		private void Update() {
-			Vector3 toPlayer = playerPos - transform.position;
+		public Transform eyes;
+
+		private float _distToPlayer;
+		private bool  _canSeePlayer;
+
+		private void FixedUpdate() {
+			_playerPos   = player.position;
+			_playerPos.y = transform.position.y;
+			
+			Vector3 toPlayer = _playerPos - transform.position;
 			_distToPlayer = toPlayer.magnitude;
 
-			Vector3 heightNormalizedPlayerPos = playerPos;
-			heightNormalizedPlayerPos.y = transform.position.y;
-			
-			transform.LookAt(heightNormalizedPlayerPos);
+			CheckVision(toPlayer);
 
-			if (_distToPlayer > minDistance && isWalking) {
+			WalkToPlayer();
+		}
+
+		private void OnDrawGizmos() {
+			//draw player pos sphere
+			Gizmos.color = Color.green;
+			Gizmos.DrawSphere(_playerPos, 0.2f);
+
+			if (!_canSeePlayer) {
+				Vector3 leftSightLineEnd =
+					eyes.position + Quaternion.Euler(0, -maxViewAngle / 2, 0) * eyes.forward * maxViewRange;
+				Vector3 rightSightLineEnd =
+					eyes.position + Quaternion.Euler(0, maxViewAngle / 2, 0) * eyes.forward * maxViewRange;
+
+				Debug.DrawLine(eyes.position, leftSightLineEnd, Color.yellow);
+				Debug.DrawLine(eyes.position, rightSightLineEnd, Color.yellow);
+			} else { Debug.DrawLine(eyes.position, _playerPos, Color.red); }
+		}
+
+		private void CheckVision(Vector3 toPlayer) {
+			toPlayer.Normalize();
+
+			if (Vector3.Angle(eyes.forward, toPlayer) <= maxViewAngle / 2) {
+				_canSeePlayer = !Physics.Raycast(eyes.position, toPlayer, _distToPlayer, ~LayerMask.GetMask("Player"));
+			}
+		}
+
+		private void WalkToPlayer() {
+			if (!isWalking || !_canSeePlayer) return;
+
+			transform.LookAt(_playerPos);
+
+			if (_distToPlayer > minDistance) {
 				Vector3 walkVec = transform.forward * speed;
 				walkVec.y          =  0;
 				transform.position += walkVec;
 			}
-		}
-
-		private void OnDrawGizmos() {
-			Gizmos.color = Color.green;
-			Gizmos.DrawSphere(playerPos, 0.2f);
 		}
 	}
 }
