efa5e44fc468a9b168c5dc8f4deb6b8b58c55700
Jackson Roberts <jacksongroberts@protonmail.com>
2023-02-24 21:17:33 -0800

    added wall & hole painting
    yes the ui is terrible but IMGUI sucks and im tired
    also no it still doesnt actually modify the tiles cuz they dont exist yet -_- (also they might not be writable? not sure have to ask emer)

--- Assets/Scripts/Levels/Editor/TerrainTool.cs
+++ Assets/Scripts/Levels/Editor/TerrainTool.cs
@@ -12,8 +12,25 @@ namespace Levels.Editor {
 		private RoomController _controller;
 		private Room           Room => _controller.Room;
 
-		private readonly Color _highlightRingColor = new(0, 1, 0, 1);
-		private readonly Color _highlightTileColor = new(0, 1, 0, 0.2f);
+		enum Settings {
+			Height,
+			Wall,
+			Hole
+		}
+
+		private Settings _settings;
+
+		private const float SettingsBoxWidth  = 100f;
+		private const float SettingsBoxHeight = 100f;
+
+		private readonly Color _heightRingColor = new(0, 1, 0, 1);
+		private readonly Color _heightTileColor = new(0, 1, 0, .2f);
+
+		private readonly Color _wallRingColor = new(0, 0, 1, 1);
+		private readonly Color _wallTileColor = new(0, 0, 1, .2f);
+
+		private readonly Color _holeRingColor = new(1, 0, 0, 1);
+		private readonly Color _holeTileColor = new(1, 0, 0, .2f);
 
 		private const float HighlightRadiusScale = 0.1f;
 		private const float HighlightRadiusMin   = 0.5f;
@@ -27,28 +44,40 @@ namespace Levels.Editor {
 
 		private List<Vector2Int> _highlightedTiles;
 
+		private bool _leftClick;
+
 		private void OnEnable() {
 			_controller = (RoomController) target;
 
 			_highlightedTiles = new List<Vector2Int>();
+
+			_settings = Settings.Height;
 		}
 
 		public override void OnToolGUI(EditorWindow window) {
 			if (Room is null) return;
 			Event e = Event.current;
 
+			//honestly no idea what this actually does under the hood, found it in a forum post
+			//but it fixed the issue of getting left click mouse events so idk
+			//https://forum.unity.com/threads/hurr-eventtype-mouseup-not-working-on-left-clicks.99909/
+			//comment from 'Amitloaf', a fair way down the thread
+			HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+
 			Raycast(e, window);
 
 			CheckInputs(e);
 
 			DrawHandles(e);
+
+			DrawGUI(window);
 		}
 
 		private void Raycast(Event e, EditorWindow window) {
 			Vector2 mousePos = e.mousePosition;
 			Ray     ray      = HandleUtility.GUIPointToWorldRay(mousePos);
 
-			if (e.shift) return;
+			if (e.shift && _settings == Settings.Height) return;
 
 			if (Physics.Raycast(ray, out RaycastHit hit, maxDistance: 100, layerMask: LayerMask.GetMask("Room"))) {
 				if (hit.transform != _controller.transform) return;
@@ -59,13 +88,43 @@ namespace Levels.Editor {
 
 				_center.Value = new Vector2(localPos.x, localPos.z);
 
-				_currentHeight = 0; //todo: replace w/ getting actual tile height (doesn't work cuz no room tile rn)
-				//_currentHeight = Room.GetTileAt((int) _center.Value.x, (int) _center.Value.y).Height;
-
 				CalculateHighlightedTiles();
+
+				switch (_settings) {
+					case Settings.Height:
+						_currentHeight = 0; //todo: replace w/ getting actual tile height (doesn't work cuz no room tile rn)
+						//_currentHeight = Room.GetTileAt((int) _center.Value.x, (int) _center.Value.y).Height;
+						break;
+					case Settings.Wall:
+						if (_leftClick) PaintWalls();
+						break;
+					case Settings.Hole:
+						if (_leftClick) PaintHoles();
+						break;
+				}
+				
+				
 			} else {
 				_center.Empty();
 				_highlightedTiles.Clear();
+			}
+		}
+
+		private Color GetRingColor() {
+			switch (_settings) {
+				case Settings.Height: return _heightRingColor;
+				case Settings.Wall:   return _wallRingColor;
+				case Settings.Hole:   return _holeRingColor;
+				default:              return Color.magenta;
+			}
+		}
+
+		private Color GetTileColor() {
+			switch (_settings) {
+				case Settings.Height: return _heightTileColor;
+				case Settings.Wall:   return _wallTileColor;
+				case Settings.Hole:   return _holeTileColor;
+				default:              return Color.magenta;
 			}
 		}
 
@@ -88,6 +147,20 @@ namespace Levels.Editor {
 			}
 		}
 
+		private void PaintWalls() {
+			foreach (Vector2Int tile in _highlightedTiles) {
+				Debug.Log("painting walls"); //todo: actually set walls, but tiles don't exist yet (& also aren't writable??)
+				//Room.GetTileAt(tile.x, tile.y) = new Room.Tile(Room.TileFlags.Wall, 0);
+			}
+		}
+
+		private void PaintHoles() {
+			foreach (Vector2Int tile in _highlightedTiles) {
+				Debug.Log("painting holes"); //todo: actually set holes, but tiles don't exist yet (& also aren't writable??)
+				//Room.GetTileAt(tile.x, tile.y) = new Room.Tile(Room.TileFlags.Pit, 0);
+			}
+		}
+
 		private void CheckInputs(Event e) {
 			if (!e.shift) return;
 
@@ -97,35 +170,68 @@ namespace Levels.Editor {
 					_highlightRadius =  Mathf.Clamp(_highlightRadius, HighlightRadiusMin, HighlightRadiusMax);
 					CalculateHighlightedTiles();
 					break;
+				case EventType.MouseDown:
+					if (e.button  == 0) _leftClick = true;
+					break;
+				case EventType.MouseUp:
+					if (e.button == 0) _leftClick = false;
+					break;
 			}
 		}
 
 		private void DrawHandles(Event e) {
 			using (new Handles.DrawingScope(_controller.transform.localToWorldMatrix)) {
-				Handles.color = _highlightTileColor;
+				Handles.color = GetTileColor();
 				foreach (Vector2Int tile in _highlightedTiles) {
 					Vector3 centerPos = new Vector3(tile.x + 0.5f, _controller.transform.position.y, tile.y + 0.5f);
 					Handles.DrawSolidDisc(centerPos, _controller.transform.up, 0.4f);
 				}
 
 				if (_center.Enabled) {
-					Handles.color = _highlightRingColor;
+					Handles.color = GetRingColor();
 					Vector3 centerPos = new Vector3(_center.Value.x, _controller.transform.position.y, _center.Value.y);
 					Handles.DrawWireDisc(centerPos, _controller.transform.up, _highlightRadius);
 
-					if (e.shift) {
+					if (e.shift && _settings == Settings.Height) {
 						Handles.color = Handles.xAxisColor;
 						Vector3 handlePos = centerPos + Vector3.up * _currentHeight;
-						_currentHeight = Mathf.FloorToInt(Handles.ScaleSlider(
-							                                  _currentHeight + 1, handlePos,
-							                                  _controller.transform.up,
-							                                  Quaternion.identity,
-							                                  HandleUtility.GetHandleSize(handlePos), 1
-						                                  )
-						                                - 1);
+						_currentHeight = Handles.ScaleSlider(
+							                 _currentHeight + 1, handlePos,
+							                 _controller.transform.up,
+							                 Quaternion.identity,
+							                 HandleUtility.GetHandleSize(handlePos), 0
+						                 )
+						               - 1;
 					}
 				}
 			}
+		}
+
+		private void DrawGUI(EditorWindow window) {
+			Handles.BeginGUI();
+
+			const float buttonHeight = SettingsBoxHeight * .8f / 3;
+
+			const float screenBottomOffset = 30f;
+
+			GUI.Box(
+				new Rect(0, window.position.height - SettingsBoxHeight - screenBottomOffset, SettingsBoxWidth,
+				         SettingsBoxHeight),
+				"Terrain Options");
+
+			if (GUI.Button(
+				    new Rect(0, window.position.height - buttonHeight * 3 - screenBottomOffset, SettingsBoxWidth,
+				             buttonHeight), "Height - G")) { _settings = Settings.Height; }
+
+			if (GUI.Button(
+				    new Rect(0, window.position.height - buttonHeight * 2 - screenBottomOffset, SettingsBoxWidth,
+				             buttonHeight), "Wall - B")) { _settings = Settings.Wall; }
+
+			if (GUI.Button(
+				    new Rect(0, window.position.height - buttonHeight * 1 - screenBottomOffset, SettingsBoxWidth,
+				             buttonHeight), "Hole - R")) { _settings = Settings.Hole; }
+
+			Handles.EndGUI();
 		}
 	}
 }
