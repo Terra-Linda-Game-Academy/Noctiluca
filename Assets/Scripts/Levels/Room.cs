6378ea9bd29f36123eb95594799f37c8bee235ba
Jackson Roberts <jacksongroberts@protonmail.com>
2023-03-12 22:44:19 -0700

    removed commented code

--- Assets/Scripts/Levels/Room.cs
+++ Assets/Scripts/Levels/Room.cs
@@ -41,24 +41,12 @@ namespace Levels {
 		public Tile GetTileAt(int x, int z) {
 			if (tileMap.Length <= 0) ResetTiles();
 
-			if (x < 0 || x >= size.x || z < 0 || z >= size.z) { //todo: remove this
-				return new Tile(TileFlags.Wall, 0);
-			}
+			if (x < 0 || x >= size.x || z < 0 || z >= size.z) return new Tile(TileFlags.Wall, 0);
 
-			/*float height = 0;
-			if (z > 6) height = 1;
-			if (x > 10) height = 2;
-			height += 0.2f * Mathf.Sin(x) * Mathf.Sin(z);
-			if (x < 0 || z < 0 || x >= size.x || z >= size.z) { //todo: remove this
-			    return new Tile(TileFlags.Wall, Mathf.Clamp(height, 0, size.y));
-			}
-			return new Tile(height >= size.y ? TileFlags.Wall : TileFlags.None, Mathf.Clamp(height, 0, size.y));*/
 			return tileMap[ToLinearIndex(x, z)];
 		}
 
 		public bool SetTileAt(Tile tile, int x, int z) {
-			//if (tileMap.Length <= 0) ResetTiles();
-
 			if (x < 0 || x >= size.x || z < 0 || z >= size.z) { return false; }
 
 			tileMap[ToLinearIndex(x, z)] = tile;
@@ -94,12 +82,12 @@ namespace Levels {
 
 		public void UpdateSize(Vector3Int newSize) {
 			Tile[] newArray = new Tile[newSize.x * newSize.z];
-			
+
 			for (int i = 0; i < newSize.x; i++) {
 				for (int j = 0; j < newSize.z; j++) {
 					int oldLinearIndex = ToLinearIndex(i, j);
 					int newLinearIndex = ToLinearIndex(i, j);
-					
+
 					if (i >= size.x || j >= size.z) {
 						newArray[newLinearIndex] = new Tile(TileFlags.None, 0f);
 						continue;
@@ -113,9 +101,7 @@ namespace Levels {
 			size    = newSize;
 		}
 
-		public int ToLinearIndex(int x, int z) {
-			return z * size.x + x;
-		}
+		public int ToLinearIndex(int x, int z) { return z * size.x + x; }
 
 		public Vector2Int FromLinearIndex(int i) {
 			int z = Math.DivRem(i, size.x, out int x);
