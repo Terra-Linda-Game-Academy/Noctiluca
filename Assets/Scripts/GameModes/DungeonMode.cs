d26128383a0084612b92f9e162859af1325397a1
Jackson Roberts <jacksongroberts@protonmail.com>
2023-05-09 14:53:59 -0700

    cleaned and moved a lot of things
    its rly messy but just trying to get things ready to merge better

--- Assets/Scripts/GameModes/DungeonMode.cs
+++ Assets/Scripts/GameModes/DungeonMode.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 namespace GameModes {
     public class DungeonGameMode : GameMode {
         [SerializeField] private LevelStack levels;
-        
-        protected override IEnumerator _OnStart() { throw new System.NotImplementedException(); }
+
+        protected override IEnumerator _OnStart() {
+            Debug.Log("going to dungeon");
+            yield break;
+        }
         protected override IEnumerator _OnEnd() { throw new System.NotImplementedException(); }
     }
 }
