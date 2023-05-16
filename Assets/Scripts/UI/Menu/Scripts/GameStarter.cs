26b22d42561dc882806e6dcab33b399ab2dc6d02
Jackson Roberts <jacksongroberts@protonmail.com>
2023-05-16 14:14:50 -0700

    it goes to the dungeon now
    theres some rando errors but it does go

--- Assets/Scripts/UI/Menu/Scripts/GameStarter.cs
+++ Assets/Scripts/UI/Menu/Scripts/GameStarter.cs
@@ -1,11 +1,8 @@
-using GameModes;
 using Main;
 using UnityEngine;
 
 namespace UI.Menu.Scripts {
 	public class GameStarter : MonoBehaviour {
-		public void StartGame() {
-			StartCoroutine(App.GameModeManager.SwitchMode(new DungeonGameMode()));
-		}
+		public void StartGame() => App.GameModeManager.StartGame();
 	}
 }
